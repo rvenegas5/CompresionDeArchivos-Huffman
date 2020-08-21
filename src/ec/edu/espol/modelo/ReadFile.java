9fb84106338b44289b9811281d17515bf54972b6
William Venegas <richardvenegasg16@gmail.com>
2020-08-20 23:16:26 -0500

    Creado clase para la lectura de archivos

---
+++ src/ec/edu/espol/modelo/ReadFile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.modelo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author WILLIAM
+ */
+public class ReadFile {
+    
+    public static Map<Character, Integer> readTXTFile(String path){
+        Map<Character, Integer> frequency = new HashMap<>();
+        
+        return frequency;
+    }
+}
