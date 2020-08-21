565deea9ec9a9989ec9bd2d348b5cd79a23aabd0
William Venegas <richardvenegasg16@gmail.com>
2020-08-20 23:45:59 -0500

    Agregando metodo para obtener la frecuencia de los caracteres

--- src/ec/edu/espol/controlador/VentanaPrincipalController.java
+++ src/ec/edu/espol/controlador/VentanaPrincipalController.java
@@ -9,16 +9,11 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 /**
--- src/ec/edu/espol/modelo/ReadFile.java
+++ src/ec/edu/espol/modelo/ReadFile.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.espol.modelo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  *
  * @author WILLIAM
@@ -16,7 +15,28 @@ public class ReadFile {
     
     public static Map<Character, Integer> readTXTFile(String path){
         Map<Character, Integer> frequency = new HashMap<>();
-        
+        try {
+            File file = new File (path);
+            FileReader fr = new FileReader (file);
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            
+            while((linea = br.readLine()) != null){
+                Character c;
+                for (int i = 0; i < linea.length(); i++){
+                    c = linea.charAt(i);
+                    if (!frequency.containsKey(c)){
+                        frequency.put(c, 1);
+                    }
+                    else{
+                        frequency.put(c, frequency.get(c) + 1);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
         return frequency;
     }
+    
 }
